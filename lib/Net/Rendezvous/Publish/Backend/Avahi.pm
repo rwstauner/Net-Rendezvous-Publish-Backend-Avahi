6d471ca3d99ca2e31ed5ff0ce25f077331e00a31
Jack Bates <ms419@freezone.co.uk>
2007-05-07 20:36:36 -0800

    import Net-Rendezvous-Publish-Backend-Avahi 0.03 from CPAN
    git-cpan-module:   Net-Rendezvous-Publish-Backend-Avahi
    git-cpan-version:  0.03
    git-cpan-authorid: JABLKO
    git-cpan-file:     authors/id/J/JA/JABLKO/Net-Rendezvous-Publish-Backend-Avahi-0.03.tar.gz

--- lib/Net/Rendezvous/Publish/Backend/Avahi.pm
+++ lib/Net/Rendezvous/Publish/Backend/Avahi.pm
@@ -7,43 +7,40 @@ use warnings;
 
 use Net::DBus;
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub new {
 	my $class = shift;
-
-	my $bus = Net::DBus->system();
-	my $service = $bus->get_service('org.freedesktop.Avahi');
-	my $server = $service->get_object('/', 'org.freedesktop.Avahi.Server');
-
-	my $self = {service => $service,
-		server => $server};
+	my $self = {@_};
 	bless $self, $class;
+
+	my $bus = Net::DBus->system;
+	$self->{service} = $bus->get_service('org.freedesktop.Avahi');
+	$self->{server} = $self->{service}->get_object(
+		'/', 'org.freedesktop.Avahi.Server');
 
 	return $self;
 }
 
-sub DESTROY {
-}
-
 sub publish {
 	my $self = shift;
-	my %params = @_;
+	my %args = @_;
 
-	my $group = $self->{service}->get_object($self->{server}->EntryGroupNew(),
-		'org.freedesktop.Avahi.EntryGroup');
+	my $group = $self->{service}->get_object(
+		$self->{server}->EntryGroupNew, 'org.freedesktop.Avahi.EntryGroup');
 	$group->AddService(Net::DBus::dbus_int32(-1), Net::DBus::dbus_int32(-1),
-		Net::DBus::dbus_uint32(0), $params{name}, $params{type},
-		$params{domain}, $params{host}, Net::DBus::dbus_uint16($params{port}),
+		Net::DBus::dbus_uint32(0), $args{name}, $args{type},
+		$args{domain}, $args{host}, Net::DBus::dbus_uint16($args{port}),
 
-		# Add Service argument signature is aay.  Split first into key/value
-		# pairs at character \x1, then map characters to bytes & add DBus type
+		# AddService argument signature is aay.  Split first into key/value
+		# pairs at character \x01, then map characters to bytes and add DBus
+		# type.
 		[map {
 			[map {
 				Net::DBus::dbus_byte(ord($_))
 			} (split //, $_)]
-		} (split /\x1/, $params{txt})]);
-	$group->Commit();
+		} (split /\x01/, $args{txt})]);
+	$group->Commit;
 
 	return $group;
 }
@@ -52,7 +49,10 @@ sub publish_stop {
 	my $self = shift;
 	my ($group) = @_;
 
-	$group->Free();
+	$group->Free;
+}
+
+sub step {
 }
 
 1;
@@ -61,14 +61,20 @@ __END__
 
 =head1 NAME
 
-Net::Rendezvous::Publish::Backend::Avahi - publish zeroconf data with the Avahi
-library
+Net::Rendezvous::Publish::Backend::Avahi - Publish zeroconf data with the Avahi
+library.
 
 =head1 DESCRIPTION
 
-This module publishes zeroconf data with the Avahi library
+This module publishes zeroconf data with the Avahi library.
 
-It is a backend for the Net::Rendezvous::Publish module
+It's a backend for the Net::Rendezvous::Publish module.
+
+=head1 PREREQUISITES
+
+Net::DBus
+
+Net::Rendezvous::Publish
 
 =head1 AUTHOR
 
@@ -76,15 +82,12 @@ Jack Bates <ms419@freezone.co.uk>
 
 =head1 COPYRIGHT
 
-Copyright 2006, Jack Bates. All rights reserved
+Copyright 2006, Jack Bates.  All rights reserved.
 
-This program is free software. You can redistribute it and/or modify it under
-the same terms as Perl itself
+This program is free software.  You can redistribute it and/or modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-Net::Rendezvous::Publish - the module this module supports
+Net::Rendezvous::Publish - The module this module supports.
 
 L<Avahi|http://avahi.org/>
-
-=cut
